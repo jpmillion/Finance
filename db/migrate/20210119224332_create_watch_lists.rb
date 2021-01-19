4a4a26cad92d81a4cfa11baeee6a8d91dc1f882a
john million <jpmillion@comcast.net>
2021-01-19 17:54:12 -0500

    added user_id to watch_lists

--- db/migrate/20210119224332_create_watch_lists.rb
+++ db/migrate/20210119224332_create_watch_lists.rb
@@ -2,6 +2,7 @@ class CreateWatchLists < ActiveRecord::Migration[6.0]
   def change
     create_table :watch_lists do |t|
       t.string :name
+      t.integer :user_id
 
       t.timestamps
     end
