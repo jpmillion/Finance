2824fe01b0283453f85d8db1b3d7cc522ad354f0
john million <jpmillion@comcast.net>
2021-01-19 18:59:42 -0500

    validates presence of name, email, username, and password presence in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :financial_products, through: :user_accounts
     has_many :watch_lists
     has_many :stocks
+
+    validates :first_name, :last_name, :username, :email, :password, presence: true
 end
