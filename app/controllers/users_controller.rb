3d7b62fdba910ed447242595b0d3facee2c35b0d
john million <jpmillion@comcast.net>
2021-01-21 20:20:34 -0500

    basic create action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            redirect_to user_path(user)
+        else
+            render new_user_path
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :admin, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :user_accounts
     has_many :financial_products, through: :user_accounts
     has_many :watch_lists
     has_many :stocks
 
-    validates :first_name, :last_name, :username, :email, :password, presence: true
+    validates :first_name, :last_name, :username, :email, :password_digest, presence: true
 end
