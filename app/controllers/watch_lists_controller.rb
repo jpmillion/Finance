f4e40b4bc85c4735237e67db3cb71b6e03efe0c9
john million <jpmillion@comcast.net>
2021-01-23 17:54:55 -0500

    form for new and edit action in watch lists

--- app/controllers/watch_lists_controller.rb
+++ app/controllers/watch_lists_controller.rb
@@ -1,2 +1,6 @@
 class WatchListsController < ApplicationController
+
+    def new
+        @watchlist = WatchList.new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   post '/auth/:provider/callback', to: 'sessions#create'
   resources :sessions, only: [:create, :destroy]
-  resources :stocks
-  resources :watch_lists
   resources :transactions
   resources :positions
   resources :financial_products
   resources :users do
     resources :user_accounts
+    resources :watch_lists
+    resources :stocks
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
