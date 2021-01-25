7e95c08918687f6ad219d5bbdf446880033e5c18
john million <jpmillion@comcast.net>
2021-01-25 07:07:09 -0500

    added validations for presence and uniqueness to financial product

--- app/models/financial_product.rb
+++ app/models/financial_product.rb
@@ -1,4 +1,7 @@
 class FinancialProduct < ApplicationRecord
     has_many :user_accounts
     has_many :users, through: :user_accounts
+
+    validates :name, :description, presence: true
+    validates :name, :description, uniqueness: true
 end
