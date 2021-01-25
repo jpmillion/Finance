83a1e054d276423cbc97ffd8cb5ac61689bf78f6
john million <jpmillion@comcast.net>
2021-01-25 07:32:20 -0500

    deleted validations for position and added validations in equity for shares and symbol

--- app/models/equity.rb
+++ app/models/equity.rb
@@ -1,2 +1,4 @@
 class Equity < Position
+    validates :symbol, presence: true
+    validates :shares, numericality: { greater_than:  0, only_integer: true }
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,5 +1,3 @@
 class Position < ApplicationRecord
     belongs_to :user_account
-    validates :symbol, presence: true
-    validates :shares, numericality: { greater_than:  0, only_integer: true }
 end
