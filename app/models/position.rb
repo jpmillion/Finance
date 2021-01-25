12ccd700793f790bb22dd349e5aafc2561a3fad2
john million <jpmillion@comcast.net>
2021-01-24 19:38:48 -0500

    numericaly for shares in position.rb

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,5 @@
 class Position < ApplicationRecord
     belongs_to :user_account
-    validates :symbol, :shares, presence: true
+    validates :symbol, presence: true
+    validates :shares, numericality: { greater_than:  0, only_integer: true }
 end
