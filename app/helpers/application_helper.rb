5e82c4e0cbb5c24ab9b4b52f7c2fe0152505427e
john million <jpmillion@comcast.net>
2021-01-24 20:43:18 -0500

    error messages for position

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
+    def messages_for_errors(model)
+        if model.errors.any?
+            tag.div id: "error_explanation" do
+                    model.errors.full_messages.each do |message|
+                        tag.li message
+                    end
 
+            end
+        end
+    end
 end
