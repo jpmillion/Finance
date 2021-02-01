c25103fda6b9c48f639fe3d1abb532879131d685
john million <jpmillion@comcast.net>
2021-02-01 08:26:45 -0500

    helper methods for number of fin prod and net worth

--- app/helpers/financial_products_helper.rb
+++ app/helpers/financial_products_helper.rb
@@ -1,3 +1,10 @@
 module FinancialProductsHelper
 
+    def display_sums_of_products(k, v) 
+        content_tag(:p, "#{k} : #{v}")
+    end
+
+    def display_net_worth_of_customer(customer)
+        content_tag(:p, "#{customer.full_name} : net worth #{number_to_currency(customer.net_worth)}")  
+    end
 end
