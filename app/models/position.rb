a52fd6046ef9caaca6a34088932368333817c890
john million <jpmillion@comcast.net>
2021-02-01 09:10:33 -0500

    moved iex api to equity model

--- app/models/position.rb
+++ app/models/position.rb
@@ -44,35 +44,4 @@ class Position < ApplicationRecord
             end
         end
     end
-
-    @@lookup = IEX::Api::Client.new(
-        publishable_token: 'pk_02f29690e23d4897b4b7916f6e78d39e',
-        secret_token: 'sk_a74468b214424b4c825232000f18608c',
-        endpoint: 'https://cloud.iexapis.com/v1'
-    )
-
-    def share_price(symbol)
-        @@lookup.quote(symbol).latest_price
-    end
-
-    def total_value(symbol)
-        self.share_price(symbol) * self.shares
-    end
-
-    def total_purchase_price(symbol, shares)
-        self.share_price(symbol) * shares
-    end
-
-    def company_name(symbol)
-        @@lookup.quote(symbol).company_name
-    end
-
-    def affordable?(symbol, shares)
-        balance, total_price = self.user_account.cash_balance, self.total_purchase_price(symbol, shares)
-        if balance > total_price 
-            self.user_account.make_payment(total_price)
-        else
-            false
-        end   
-    end
 end
