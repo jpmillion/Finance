a01dde5a286c2c4eb0aef5242c61ef60b9ad3408
john million <jpmillion@comcast.net>
2021-02-01 07:07:56 -0500

    deleted iex instance and unused authorization checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,28 +15,11 @@ class ApplicationController < ActionController::Base
 
     def login_required
         if !logged_in?
-            redirect_to login_path, alert: "**Login Required!!"
-        # elsif params[:user_id]
-        #     if params[:user_id].to_i != session[:user_id]
-        #         redirect_to user_path(session[:user_id]), alert: "Access Denied"
-        #     end
-        # elsif params[:user_account_id]
-        #     if !current_user.user_accounts.include?
-        #         redirect_to user_path(session[:user_id]), alert: "Access Denied"
-        #     end  
+            redirect_to login_path, alert: "**Login Required!!" 
         end
     end
 
     def not_authorization
         redirect_to current_user, alert: "**Not Authorized to View this page"
-    end
-
-    def stock_quote(symbol, shares)
-        stock = IEX::Api::Client.new(
-            publishable_token: 'pk_02f29690e23d4897b4b7916f6e78d39e',
-            secret_token: 'sk_a74468b214424b4c825232000f18608c',
-            endpoint: 'https://cloud.iexapis.com/v1'
-        ).quote(symbol)
-        { symbol: stock['symbol'], name: stock['company_name'], value: (stock['latest_price']*shares.to_f).round(2), shares: shares }
     end
 end
