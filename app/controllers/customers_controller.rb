306a0b460ef3e0778282e5a1458f3af61616177a
john million <jpmillion@comcast.net>
2021-02-03 07:22:08 -0500

    actions create, show, edit, update, and destroy in customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,22 @@
 class CustomersController < ApplicationController
+
+    def create
+        
+    end
+
+    def show
+        
+    end
+
+    def edit
+        
+    end
+
+    def update
+        
+    end
+
+    def destroy
+        
+    end
 end
