4800cb892ceaf9a9b48f4d18613337f78c587713
john million <jpmillion@comcast.net>
2021-02-03 07:37:05 -0500

    customer params in customer controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,22 @@
 class AdminsController < ApplicationController
+
+    def create
+        
+    end
+
+    def show
+        
+    end
+
+    def edit
+        
+    end
+
+    def update
+        
+    end
+
+    def destroy
+        
+    end
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -19,4 +19,10 @@ class CustomersController < ApplicationController
     def destroy
         
     end
+
+    private
+
+    def customer_params
+        params.require(:customer).permit(:first_name, :last_name, :username, :email, :admin, :password, :password_confirmation, :type)
+    end
 end
