f96f61d53318eb51777f2fbb191d75a914f04a5d
john million <jpmillion@comcast.net>
2021-03-04 15:56:54 -0500

    user only validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
     has_many :positions, through: :user_accounts
 
     #validates :first_name, :last_name, :username, :email, presence: true
-    validates :email, presence: true
-    validates :username, :email, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 
     # before_validation :full_name
 
